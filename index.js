d0a9493a8281bbc370f67f515aaf73480dc71f29
Sohvi Puska <sohvip1@gmail.com>
2023-09-11 20:12:23 +0300

    cors & port modification

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const fetch = require('node-fetch')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('person', (request) => {
     if (request.method === 'POST') {
@@ -100,7 +102,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
